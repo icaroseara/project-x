f6fc1840a3162972bfa34d38f9260c537f197bdb
Icaro Seara <ikro89@gmail.com>
2018-06-03 20:48:28 -0300

    Collect creditcard and account transactions

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -15,28 +15,42 @@ PROCESSED_DATA_DIR = 'data/processed'
 
 
 def _download_external_data():
-    externa_data_file = '{}/card_statements.csv'.format(EXTERNAL_DATA_DIR)
-    if not os.path.exists(externa_data_file):
-        nu = Nubank(os.environ.get('NUBANK_LOGIN'), os.environ.get('NUBANK_PASSWORD'))
-        _card_statements_to_csv(nu.get_card_statements(), externa_data_file)
+    nu = Nubank(os.environ.get('NUBANK_LOGIN'), os.environ.get('NUBANK_PASSWORD'))
+    card_transactions = nu.get_card_statements()
+    _transactions_to_csv(card_transactions, 'card_transactions')
+
+    account_transactions = nu.get_account_statements()
+    _transactions_to_csv(account_transactions, 'account_transactions')
 
 
-def _card_statements_to_csv(card_statements, externa_data_file):
-    with open(externa_data_file, 'w') as f:
-        field_names = card_statements[0].keys()
+def _transactions_to_csv(transactions, filename):
+    field_names = transactions[0].keys() if transactions else []
+    with open('{}/{}.csv'.format(EXTERNAL_DATA_DIR, filename), 'w') as f:
         dict_writer = csv.DictWriter(f, field_names)
         dict_writer.writeheader()
-        dict_writer.writerows(card_statements)
+        dict_writer.writerows(transactions)
 
 
-def _create_raw_data_file(external_data_file):
-    raw_data_file = '{}/card_statements.csv'.format(RAW_DATA_DIR)
+def _create_raw_data_files():
+    _create_raw_data_file('card_transactions')
+    _create_raw_data_file('account_transactions')
+
+
+def _create_processed_data_files():
+    _create_processed_data_file('card_transactions')
+    _create_processed_data_file('account_transactions')
+
+
+def _create_raw_data_file(filename):
+    external_data_file = '{}/{}.csv'.format(EXTERNAL_DATA_DIR)
+    raw_data_file = '{}/{}.csv'.format(RAW_DATA_DIR)
     if not os.path.exists(raw_data_file):
         copyfile(external_data_file, raw_data_file)
 
 
-def _create_processed_data_file(raw_data_file):
-    processed_data_file = '{}/card_statements.csv'.format(PROCESSED_DATA_DIR)
+def _create_processed_data_file(filename):
+    raw_data_file = '{}/{}.csv'.format(RAW_DATA_DIR)
+    processed_data_file = '{}/{}.csv'.format(PROCESSED_DATA_DIR)
     if not os.path.exists(processed_data_file):
         copyfile(raw_data_file, processed_data_file)
 
@@ -51,10 +65,10 @@ def main():
     _download_external_data()
 
     logger.info('making raw data from external data')
-    _create_raw_data_file('{}/card_statements.csv'.format(EXTERNAL_DATA_DIR))
+    _create_raw_data_files()
 
     logger.info('making final data set from raw data')
-    _create_processed_data_file('{}/card_statements.csv'.format(RAW_DATA_DIR))
+    _create_processed_data_files()
 
 
 if __name__ == '__main__':

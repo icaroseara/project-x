08cee82afb58a432eccaa1ef90c1a090ce6a776c
Icaro Seara <ikro89@gmail.com>
2018-06-03 17:32:18 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='POC Project X',
+    author='Icaro Seara',
+    license='',
+)
